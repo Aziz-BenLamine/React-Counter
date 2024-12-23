697f21b206277b9bbf4bd199a3002c23ebc90cf1
Aziz-BenLamine <azizbenlamine@gmail.com>
2024-12-23 18:00:32 +0100

    Add app logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import Counter from "./components/Counter";
+import "./style.css";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Counter</h1>
+      </header>
+      <main>
+        <Counter />
+      </main>
+      <footer>
+        <p>Made by Aziz Ben Lamine</p>
+        <a
+          href="https://github.com/Aziz-BenLamine"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
